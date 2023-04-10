12b8442127d3ec2bf726e13f8c0d5fe858461acc
karankataria <karankatariac22@gmail.com>
2023-04-09 21:24:42 -0400

    added

--- romilportfolio/src/Components/Education/Education.js
+++ romilportfolio/src/Components/Education/Education.js
@@ -6,8 +6,19 @@ const Education = () => {
   return (
     <div className="education-container">
       <h1>Education</h1>
-      <EducationCard></EducationCard>
-      <EducationCard></EducationCard>
+      <EducationCard
+        imgAdd="https://img.collegedekhocdn.com/media/img/institute/crawled_images/273A1129.jpg"
+        title="B.Tech In Civil Engineering"
+        intership="true"
+        description="I completed my four years Bachelor of Technology in civil Engineering field from GSFC University. June 2016 to June 2020. 
+While doing my B.Tech course, I was involved in the three different internships to gain more knowledge in field."
+      ></EducationCard>
+      <EducationCard
+        intership="false"
+        imgAdd="https://www-assets.conestogac.on.ca/images/corporate-website-2019/images/about/campuses/cambridge-reuter.jpg"
+        title="PGD in Structural Steel Management And Detailing"
+        description="This dynamic two-year graduate certificate course can lead me to be specialized and will provide thorough training in the detailing, supply, fabrication and installation of steel structures including the management and coordination of projects. The training is directly related to industry standards and working practices within North America and will enable me to develop detailed drawing, analytical and communication skills for the structural steel industry."
+      ></EducationCard>
     </div>
   );
 };
